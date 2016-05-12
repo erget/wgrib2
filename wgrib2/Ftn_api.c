9dd786e687f4d405ad211c7ea290cfd89c743773
Daniel Lee <Daniel.Lee@dwd.de>
2016-05-12 14:52:38 +0200

    First commit

---
+++ wgrib2/Ftn_api.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include "grb2.h"
+#include "wgrib2.h"
+#include "fnlist.h"
+#include "grid_id.h"
+
+/* Ftn_api.c   5/2015 Public Domain Wesley Ebisuzaki
+ *
+ * this routine is called by grb2_inq - returns ofen needed information
+ *
+ */
+
+extern enum output_order_type output_order;
+extern unsigned int npnts;
+extern int nx, ny;
+extern int submsg, msg_no, inv_no;
+long int pos;
+/*
+ * HEADER:100:ftn_api_fn0:inv:0:n npnts nx ny msg_no submsg i8,5(1x,i8)
+ */
+int f_ftn_api_fn0(ARG0) {
+    /* ndata data may be undefined when data is not decoded */
+    if (mode >= 0) sprintf(inv_out, "%8u %8d %8d %8d %8d %8d",inv_no,npnts,nx,ny,msg_no, submsg);
+    return 0;
+}
+
---
+++ wgrib2/junk.h
@@ -0,0 +1,3 @@
+int f_mem_del(int mode, unsigned char **sec, float *data, unsigned int ndata, char *inv, void **local, const char *arg1);
+int f_mem_final(int mode, unsigned char **sec, float *data, unsigned int ndata, char *inv, void **local, const char *arg1, const char *arg2);
+int f_mem_init(int mode, unsigned char **sec, float *data, unsigned int ndata, char *inv, void **local, const char *arg1, const char *arg2);
